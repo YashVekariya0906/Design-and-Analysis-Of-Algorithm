74889f803a049368e1daaa7ef7238b1b92c2ced9
YashVekariya0906 <155796284+YashVekariya0906@users.noreply.github.com>
2025-09-03 10:00:54 +0530

    Create Bubble Sort.cpp

---
+++ Bubble Sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j+1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[] = {4, 5, 3, 1, 2, 8};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    bubble_sort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
