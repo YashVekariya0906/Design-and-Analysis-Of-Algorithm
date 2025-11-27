a1ce8a1ec70d574dc652b42b52861ba7b97ed7b9
YashVekariya0906 <155796284+YashVekariya0906@users.noreply.github.com>
2025-11-27 21:21:22 +0530

    Create Power Divide & Conquer O(n logn).cpp

---
+++ Power Divide & Conquer O(n logn).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+long long power(long long x, long long n) {
+    if (n == 0)
+        return 1;
+
+    long long half = power(x, n / 2);
+
+    if (n % 2 == 0)
+        return half * half;
+    else
+        return x * half * half;
+}
+
+int main() {
+    long long x, n;
+
+    cout << "Enter Base: ";
+    cin >> x;
+
+    cout << "Enter Exponent: ";
+    cin >> n;
+
+    cout << "Result: " << power(x, n) << endl;
+
+    return 0;
+}
